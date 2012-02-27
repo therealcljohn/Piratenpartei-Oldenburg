0997fa7cf19ae466d12d604670178477edce0210
Clemens John <clemens-john@gmx.de>
2012-02-27 11:48:27 +0100

    Add Piratenmk Wordpress Theme from http://piratenpartei-iserlohn.de/wordpress_theme/

---
+++ piratenmk/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+if( function_exists('register_sidebar') )
+	register_sidebar( array( 'name' => 'sidebar_full_left' ) );
+if( function_exists('register_sidebar') )
+	register_sidebar( array( 'name' => 'sidebar_full_right' ) );
+?>
